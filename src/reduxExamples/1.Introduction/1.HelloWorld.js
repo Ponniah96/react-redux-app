4114fb98b87f39fa964a2f2f2a80c2f0629ee2a4
unknown <ponniahk78@gmail.com>
2023-03-05 18:37:46 +0530

    Code Snippet Integration

--- src/reduxExamples/1.Introduction/1.HelloWorld.js
+++ src/reduxExamples/1.Introduction/1.HelloWorld.js
@@ -1,16 +1,100 @@
 import { useSelector,useDispatch } from "react-redux";
 import { actionType } from "../../redux/actions/actions";
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { atelierCaveDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import "../../App.css"
 export default function HelloWorld(){
     const state = useSelector((state) => state);
     const stateValue=state.helloWorld
     const dispatch=useDispatch();
-    console.log(state);
+    const codeString = `
+HelloWorld.js
+
+import { useSelector,useDispatch } from "react-redux";
+import { actionType } from "../../redux/actions/actions";
+export default function HelloWorld(){
+    const state = useSelector((state) => state);
+    const stateValue=state.helloWorld
+    const dispatch=useDispatch();
     return(
         <>
             <p>Welcome to Redux Basic Example</p>
             <p>While click below button, redux state are updated with the value hello world</p>
             <button onClick={()=>dispatch(actionType())} >Click Here to update Redux State</button>
             {stateValue.value !== ''?<p>Redux Store Value: <b>{stateValue.value}</b></p>:<p>Empty Redux State Value</p>}
+        </>
+    )
+}
+
+Store.js
+
+import {createStore, applyMiddleware, compose, combineReducers} from "redux";
+import Reducer from "./redux/reducers/reducers";
+import Reducers1 from "./redux/reducers/reducers1";
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__|| compose;
+const reducers=combineReducers({
+    helloWorld:Reducer,
+    personalDetails: Reducers1})
+const store = createStore(reducers,composeEnhancers(applyMiddleware()));
+// const store = createStore(Reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+export default store;
+
+actions.js
+
+import { HELLO_WORLD } from "../actionTypes/actionTypes";
+
+const actionType=()=>{
+    return{
+        type: HELLO_WORLD
+    }
+}
+
+export {actionType}
+
+actionTypes.js
+
+export const HELLO_WORLD="Hello World";
+
+reducers.js
+
+import { HELLO_WORLD } from "../actionTypes/actionTypes";
+
+const InitialState={
+    value: ""
+};
+
+const Reducer = (state=InitialState, action)=>{
+    switch(action.type){
+        case HELLO_WORLD:  return {...state, value:"Hello World"};
+        default : return state;
+    }
+}
+
+export default Reducer
+
+    `
+    const copyFunction=(buttonID, functionID)=>{
+        var copyText = document.getElementById(functionID)
+        navigator.clipboard.writeText(copyText.innerText);
+        var buttonElement=document.getElementById(buttonID);
+        buttonElement.innerHTML= '<i class="bi bi-check2"></i> Copied';
+    }                    
+    return(
+        <>
+            <div className="flex-body">
+                <div className="flex-left-content">
+                    <p>Welcome to Redux Basic Example</p>
+                    <p>While click below button, redux state are updated with the value hello world</p>
+                    <button onClick={()=>dispatch(actionType())} >Click Here to update Redux State</button>
+                    {stateValue.value !== ''?<p>Redux Store Value: <b>{stateValue.value}</b></p>:<p>Empty Redux State Value</p>}
+                </div>
+                <div className="flex-right-content">
+                    <button className="copy-icon" id="redux-intro"  onClick={()=>{copyFunction('redux-intro','redux-intro-code')}}><i className="bi bi-clipboard"></i> Copy</button>
+                    <SyntaxHighlighter language="javascript" style={atelierCaveDark} id="redux-intro-code" >
+                        {codeString}
+                    </SyntaxHighlighter>
+                </div>
+            </div>
         </>
     )
 }
--- src/reduxExamples/1.Introduction/2.DataTypes.js
+++ src/reduxExamples/1.Introduction/2.DataTypes.js
@@ -1,5 +1,17 @@
 import { useSelector, useDispatch } from "react-redux";
 import { personalDetails } from "../../redux/actions/actions1";
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { atelierCaveDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import "../../App.css";
+export default function PersonalDetails(){
+    const state = useSelector((state) => state);
+    const stateValue=state.personalDetails;
+    const dispatch=useDispatch();
+    const codeString =`
+Datatypes.js
+
+import { useSelector, useDispatch } from "react-redux";
+import { personalDetails } from "../../redux/actions/actions1";
 export default function PersonalDetails(){
     const state = useSelector((state) => state);
     const stateValue=state.personalDetails;
@@ -16,6 +28,82 @@ export default function PersonalDetails(){
                 <span>Working Status:<b>{stateValue.value.workingStatus?"Active":""}</b></span><br></br>
             </p>)
             :(<p>No data Found</p>)}
+        </>
+    )
+}
+
+store.js
+
+import {createStore, applyMiddleware, compose, combineReducers} from "redux";
+import Reducer from "./redux/reducers/reducers";
+import Reducers1 from "./redux/reducers/reducers1";
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__|| compose;
+const reducers=combineReducers({
+    helloWorld:Reducer,
+    personalDetails: Reducers1})
+const store = createStore(reducers,composeEnhancers(applyMiddleware()));
+export default store;
+
+actions.js
+
+import { PERSONAL_DETAILS } from "../actionTypes/actionTypes1";
+export const personalDetails=()=>{
+    return{
+        type: PERSONAL_DETAILS
+    }
+}
+
+
+actionTypes.js
+
+export const PERSONAL_DETAILS="PERSONAL_DETAILS"
+
+reducers.js
+
+import { PERSONAL_DETAILS } from "../actionTypes/actionTypes1";
+const initialState ={
+    value:{}
+}
+const Reducers1=(state=initialState, actions)=>{
+    switch(actions.type){
+        case PERSONAL_DETAILS: return
+        {...state, 
+            value:
+            {name:"Ponniah", age:26, role:"Senior Software Engineer",workingStatus:true }};
+        default : return state;
+    }
+}
+
+export default Reducers1
+    `
+    const copyFunction=(buttonID, functionID)=>{
+        var copyText = document.getElementById(functionID)
+        navigator.clipboard.writeText(copyText.innerText);
+        var buttonElement=document.getElementById(buttonID);
+        buttonElement.innerHTML= '<i class="bi bi-check2"></i> Copied';
+    }
+    return(
+        <>
+            <div className="flex-body">
+                <div className="flex-left-content">
+                    <p>My Peronsal Details</p>
+                    <p>Click below button to display my basic details</p>
+                    <button onClick={()=>{dispatch(personalDetails())}}>Get Personal Details</button>
+                    {stateValue.value !==''?
+                        (<p> <span>Name         :<b> {stateValue.value.name}</b></span><br></br>
+                            <span>Age           :<b>  {stateValue.value.age}</b></span><br></br>
+                            <span>Role          :<b>   {stateValue.value.role}</b></span><br></br>
+                            <span>Working Status:<b>{stateValue.value.workingStatus?"Active":""}</b></span><br></br>
+                        </p>)
+                        :(<p>No data Found</p>)}
+                </div>
+                <div className="flex-right-content">
+                    <button className="copy-icon" id="redux-intro"  onClick={()=>{copyFunction('redux-intro','redux-intro-code')}}><i className="bi bi-clipboard"></i> Copy</button>
+                    <SyntaxHighlighter language="javascript" style={atelierCaveDark} id="redux-intro-code" >
+                        {codeString}
+                    </SyntaxHighlighter>
+                </div>
+            </div>
         </>
     )
 }
